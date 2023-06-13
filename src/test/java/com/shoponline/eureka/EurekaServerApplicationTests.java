be375445b0f09acad4ed424b3ba0afbc1a381bdc
Paranoj <e.behterev@yandex.ru>
2023-06-13 22:34:12 +0700

    init

---
+++ src/test/java/com/shoponline/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoponline.eureka;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Disabled
+	void contextLoads() {
+	}
+
+}
